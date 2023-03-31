0e1230f80a6f3dc84020f84721fc4b8fd812cf2c
krizis <monkriz1991@gmail.com>
2023-03-31 14:18:06 +0300

    twic

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,3 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
-
-
 export default defineNuxtConfig({
     runtimeConfig: {
         public: {
@@ -8,67 +5,76 @@ export default defineNuxtConfig({
         }
     },
     app: {
-        head: {
-            title: 'Frese - Балансировка и регулирование в РБ',
-            htmlAttrs: {
-              lang: 'ru'
-            },
-            meta: [
-              { charset: 'utf-8' },
-              { name: 'yandex-verification', content: '56b4df5279a6bb79' },
-              { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-              { hid: 'description', name: 'description', content: 'Frese' },
-              {
-                hid: 'og:title',
-                property: 'og:title',
-                content: 'Frese - Балансировка и регулирование в РБ',
-            },
-            {
-                hid: 'og:description',
-                property: 'og:description',
-                content: 'На сайте frese.by Вы можете ознакомиться с номенклатурой производителя Frese импортируемой в Республику Беларусь компанией Архитрем ( arhiterm.by ).',
-            },
-            {
-                hid: 'og:url',
-                property: 'og:url',
-                content: `https://frese.by`,
-            },
-            {
-              hid: 'og:locale',
-              property: 'og:locale',
-              content: 'ru_RU',
-            },
-            {
-              hid: 'og:type',
-              property: 'og:type',
-              content: 'website',
-            }, 
-            ],
+      head: {
+          title: 'Frese - Балансировка и регулирование в РБ',
+          htmlAttrs: {
+            lang: 'ru'
           },
-          
-        },
-    modules: [
-        'nuxt-icon',
-        'nuxt-swiper',
-        ['@nuxtjs/google-fonts', {
-          families: {
-            Montserrat: [100,300,400,500,600,700,800],
-            download: true,
-            inject: true
-          }
-        }],
-        [
-          'yandex-metrika-module-nuxt3',
+          meta: [
+            { charset: 'utf-8' },
+            { name: 'yandex-verification', content: '56b4df5279a6bb79' },
+            { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+            { hid: 'description', name: 'description', content: 'Frese' },
+            {
+              hid: 'og:title',
+              property: 'og:title',
+              content: 'Frese - Балансировка и регулирование в РБ',
+          },
           {
-            id: '93020253',
-            webvisor: true,
-          }
-        ]
-    ],
+              hid: 'og:description',
+              property: 'og:description',
+              content: 'На сайте frese.by Вы можете ознакомиться с номенклатурой производителя Frese импортируемой в Республику Беларусь компанией Архитрем ( arhiterm.by ).',
+          },
+          {
+              hid: 'og:url',
+              property: 'og:url',
+              content: `https://frese.by`,
+          },
+          {
+            hid: 'og:locale',
+            property: 'og:locale',
+            content: 'ru_RU',
+          },
+          {
+            hid: 'og:type',
+            property: 'og:type',
+            content: 'website',
+          }, 
+          ],
+        },
+    },
+    modules: [
+      '@twicpics/components/nuxt3',
+      'nuxt-icon',
+      'nuxt-swiper',
+      ['@nuxtjs/google-fonts', {
+        families: {
+          Montserrat: [100,300,400,500,600,700,800],
+          download: true,
+          inject: true
+        }
+      }],
+      [
+        'yandex-metrika-module-nuxt3',
+        {
+          id: '93020253',
+          webvisor: true,
+        }
+      ],
+  ],
+  twicpics: {
+    domain: `https://frese.by.twic.pics`,
+  },
     css:[
         "@/node_modules/bulma/css/bulma.css",
         "@/assets/main.css",
         '@/node_modules/@swoga/vue-collapsible-panel/dist/vue-collapsible-panel.css',
         '@/node_modules/vue3-photo-preview/dist/index.css'
     ],
+    build: { 
+    },
+    vite: {
+        optimizeDeps: {
+        }
+    },
 })
