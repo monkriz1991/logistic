d0e282f843dbaa09598abe7b38edca39df2d8b5f
krizis <monkriz1991@gmail.com>
2024-01-01 15:37:37 +0300

    in

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,12 +1,12 @@
 <template>
-    <div>
-        <footer class="footer">
-            <div class="content has-text-centered">
-                <p>
-                <strong>Frese.by</strong>
-                </p>
-                <p>.</p>
-            </div>
-        </footer>
-    </div>
+  <div>
+    <footer class="footer">
+      <div class="content has-text-centered">
+        <p>
+          <strong>/ Frese.by</strong>
+        </p>
+        <p>.</p>
+      </div>
+    </footer>
+  </div>
 </template>
